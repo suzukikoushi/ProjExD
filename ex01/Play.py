f6f5ad2a24c5cdf34da818881cbeeff1c29748b1
Koushi <c0a2106816@edu.teu.ac.jp>
2022-06-14 18:03:10 +0900

    課題

--- ex01/Play.py
+++ ex01/Play.py
@@ -1,6 +1,20 @@
 import random
+moji=10
+kesson=2
 def wards():
     w=[]
+    t=[]
     for i in range(26):
         w += chr(i+65)
-    return w
+    n=25
+    for j in range(moji):
+        x=w[random.randint(0,n)]
+        t+=x
+        del x
+        n-1
+    return(t)
+def b(t):
+    k=[]
+    for i in range(kaisuu):
+         k+=t[random.randint(0,len(t))]
+    return(k)

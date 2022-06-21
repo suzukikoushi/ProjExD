c202fbd253c8bfa94b37042c50c085a011aa05ce
Koushi <c0a2106816@edu.teu.ac.jp>
2022-06-21 16:18:03 +0900

    完成版(改良)

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,27 +6,38 @@ if __name__ =="__main__":
     def button_click(event):
         btn=event.widget
         num=btn["text"]
-
+        if num=="=":
+            eqn=entry.get()
+            entry.delete(0,tk.END)
+            res=eval(eqn)
+            entry.insert(tk.END,res)
+        elif num=="C":
+            entry.delete(0,tk.END)
+        elif num=="×":
+            num="*"
+            entry.insert(tk.END,num)
+        elif num=="÷":
+            num="/"
+            entry.insert(tk.END,num)
+        else:
+            entry.insert(tk.END,num)
         #tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
 
     root=tk.Tk()
-    #root.geometryがない場合ちょうどいいサイズに勝手になる
+    #root.geometry(300,500)
     r=1
-    c=0
-    for i in range(9,-1,-1):
+
+    for c,i in enumerate(["","","","C",7,8,9,"+",4,5,6,"-",1,2,3,"×",0,"","=","÷"]):
         button=tk.Button(root,
                       text=f"{i}",
-                      width=4,
+                      width=3,
                       height=1,
                       font=("Times New Roman",30))
         button.bind("<1>",button_click)
-        button.grid(row=r,column=c)
-        c+=1
-        if (i-1)%3==0:
-            r+=1
-            c=0
+        x=c%4+1
+        y=c//4+1
+        button.grid(row=y,column=x)
 
-    entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
+    entry=tk.Entry(root,justify="right",width=6,font=("Times New Roman",40))
     entry.grid(row=0,column=0,columnspan=3)
-    entry.insert(tk.END,"num")
     root.mainloop()

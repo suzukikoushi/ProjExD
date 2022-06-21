a9aa0bb78ba39bfac323e9bf7eed42f5f585239d
Koushi <c0a2106816@edu.teu.ac.jp>
2022-06-21 15:27:08 +0900

    未完成

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,12 +5,13 @@ if __name__ =="__main__":
     
     def button_click(event):
         btn=event.widget
-        txt=btn["text"]
-        tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+        num=btn["text"]
+
+        #tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
 
     root=tk.Tk()
-    root.geometry("300x500")
-    r=0
+    #root.geometryがない場合ちょうどいいサイズに勝手になる
+    r=1
     c=0
     for i in range(9,-1,-1):
         button=tk.Button(root,
@@ -24,4 +25,8 @@ if __name__ =="__main__":
         if (i-1)%3==0:
             r+=1
             c=0
+
+    entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=3)
+    entry.insert(tk.END,"num")
     root.mainloop()

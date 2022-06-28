ffc6af8ed552af37519ba5826cedbc489daec30e
Koushi <c0a2106816@edu.teu.ac.jp>
2022-06-28 17:08:11 +0900

    ４限おわり

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,18 +1,19 @@
 
 import tkinter as tk
 import maze_maker as mm
-
+import random
+import sys
 def key_down(event):
-    global key
-    key=event.keysym
+    global key #グローバル関数
+    key=event.keysym #キーを押したとき
 
 def key_up(event):
     global key
-    key=" "
+    key=" "#キーを押さなかった時
 
 def main_proc():
     global cx,cy,my,mx
-        
+        #移動する処理
     if key=="Right" and maze_bg[my][mx+1]==0:
         mx+=1
     elif key=="Left" and maze_bg[my][mx-1]==0:
@@ -23,21 +24,38 @@ def main_proc():
         my+=1
     cx,cy=mx*100+50,my*100+50
     canvas.coords("tori",cx,cy)
+    gall()
     root.after(100,main_proc)
-
+def gall():
+    global mx,my
+    #クリア処理
+    if mx==x-2 and my==y-2:
+        maze_bg[my+1][mx]=1
+        maze_bg[my-1][mx]=1
+        maze_bg[my][mx+1]=1
+        maze_bg[my][mx-1]=1
+        if key=="space":
+            sys.exit()
 if __name__ =="__main__":
     root=tk.Tk()
     root.title("迷えるこうかとん")
 
-    canvas=tk.Canvas(root,width=1500,height=900,bg="black")
+    canvas=tk.Canvas(root,width=1700,height=1100,bg="black")#ウィンドウを作る
     canvas.pack()
-
-    maze_bg=mm.make_maze(15,9)
+    x,y=15,9    #迷路の大きさ
+    maze_bg=mm.make_maze(x,y)
     mm.show_maze(canvas,maze_bg)
-
-    tori = tk.PhotoImage(file="fig/2.png")
+    #こうかとんのillustration
+    illust=["fig/0.png","fig/1.png","fig/2.png",
+            "fig/3.png","fig/4.png","fig/5.png",
+            "fig/6.png","fig/7.png","fig/8.png","fig/9.png"]
+    #ランダムでillustrationを表示
+    num=random.randint(0,9)
+    tori = tk.PhotoImage(file=illust[num])
+    #移動する量
     mx,my=1,1
-    cx,cy=mx*100+50,my*100+50
+    cx,cy=mx*100,my*100
+    #illustrationの表示位置
     canvas.create_image(mx,my,image=tori,tag="tori")
 
     key=" "
